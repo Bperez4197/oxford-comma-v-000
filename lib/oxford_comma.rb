a10a5ebce0ac03467aa27110f8c696484483d9ac
Bperez4197 <bryceperez31@gmail.com>
2017-11-29 10:35:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else array.length > 2
-     array.join(",") << (" and ")
+     array.join(",") + ", and " + last.to_s
   end
 end
